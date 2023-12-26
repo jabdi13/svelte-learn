f284cd7b3d013132226e9b7ed8b0a8c611c695ac
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-26 17:39:17 -0600

    Updating arrays and objects

---
+++ src/part1-basic-svelte/mod2-reactivity/Unity4UpdatingArraysAndObjects.svelte
@@ -0,0 +1,30 @@
+<script>
+  let numbers = [1, 2, 3, 4];
+  const obj = { foo: { bar: 1 } };
+  const foo = obj.foo;
+
+  function addNumber() {
+    // numbers.push(numbers.length + 1);
+    // Idiomatic solution
+    numbers = [...numbers, numbers.length + 1];
+  }
+
+  function updateObj() {
+    // This won't trigger reactivity on obj.foo.bar
+    // foo.bar = 2
+    // Unless you assign it like this
+    // obj = obj
+    // or this
+    obj.foo.bar = 2;
+  }
+
+  $: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
+</script>
+
+<p>{numbers.join(" + ")} = {sum}</p>
+
+<button on:click={addNumber}> Add a number </button>
+
+<p>bar = {obj.foo.bar}</p>
+
+<button on:click={updateObj}> Update obj </button>
