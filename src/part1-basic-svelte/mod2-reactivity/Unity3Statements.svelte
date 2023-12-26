6e133afd987475645ac6887949841fe5fcb2e594
Abdiel Ortega <nathanabdiel1@gmail.com>
2023-12-26 14:32:53 -0600

    Reactive statements

---
+++ src/part1-basic-svelte/mod2-reactivity/Unity3Statements.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+
+  $: if (count >= 10) {
+    alert(`count is dangerously high!`);
+    count = 0;
+  }
+</script>
+
+<button on:click={handleClick}>
+  Clicked {count}
+  {count === 1 ? "time" : "times"}
+</button>
